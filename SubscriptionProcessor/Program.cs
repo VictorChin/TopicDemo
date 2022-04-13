37ee28ff2058e6ab2bf62845bd74d99a7abec15c
Victor Chin <victor@objectmage.com>
2022-04-13 13:05:46 -0400

    Add project files.

---
+++ SubscriptionProcessor/Program.cs
@@ -0,0 +1,52 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace SubscriptionProcessor
+{
+    class Program
+    {
+        static string connectionString =
+                             "Endpoint=sb://vc0413.servicebus.windows.net/;SharedAccessKeyName=vcsub;SharedAccessKey=WBKuR5avcSD8C/1R1Dke4XyCmTBZxf0YWRfYcN9Z5dg=;EntityPath=demotopic";
+        static string topicName = "demotopic";
+
+        // name of the subscription to the topic
+        static string subscriptionName = "vcsub";
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient client;
+
+        // the processor that reads and processes messages from the subscription
+        static ServiceBusProcessor processor;
+        static async Task Main(string[] args)
+        {
+            client = new ServiceBusClient(connectionString);
+            processor = client.CreateProcessor(topicName, subscriptionName, new ServiceBusProcessorOptions());
+            processor.ProcessMessageAsync += MessageHandler;
+            processor.ProcessErrorAsync += ErrorHandler;
+            await processor.StartProcessingAsync();
+            Console.WriteLine("Wait for a minute and then press any key to end the processing");
+            Console.ReadKey();
+
+            // stop processing 
+            Console.WriteLine("\nStopping the receiver...");
+            await processor.StopProcessingAsync();
+            Console.WriteLine("Stopped receiving messages");
+        }
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body} from subscription: {subscriptionName}");
+
+            // complete the message. messages is deleted from the subscription. 
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        // handle any errors when receiving messages
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TopicDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TopicDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string connectionString = 
+                "Endpoint=sb://vc0413.servicebus.windows.net/;SharedAccessKeyName=sender;SharedAccessKey=PTQTOgkIKw6IMHDkmIjzyN8seH/dfs/vmBK1kxutrik=;EntityPath=demotopic";
+            string topicName = "demotopic";
+
+            await using var client = new ServiceBusClient(connectionString);
+            ServiceBusSender sender = client.CreateSender(topicName);
+            ServiceBusMessage message = new ServiceBusMessage("Hello From Victor");
+            message.ApplicationProperties.Add("Priority", "1");
+
+            await sender.SendMessageAsync(message);
+
+            IList<ServiceBusMessage> aBatch = new List<ServiceBusMessage>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var msg = new ServiceBusMessage($"Hello From Victor-{i}");
+                msg.ApplicationProperties.Add("OrderNumber", i);
+                aBatch.Add(msg);
+            }
+            await sender.SendMessagesAsync(aBatch);
+        }
+    }
+}
